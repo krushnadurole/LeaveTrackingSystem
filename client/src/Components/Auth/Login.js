9ee33c3c0e8712da124254d4078a55b1c42cce46
krushnadurole2000ad <krushnadurole2000ad@gmail.com>
2024-07-23 23:21:17 +0530

    Updated the client

---
+++ client/src/Components/Auth/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { json, useNavigate } from 'react-router-dom'
+import '../Auth/Login.css'
+
+const Login = (props) => {
+
+  const [credentials, setcredientials] = useState({ email: "", password: "" });
+  const navigate = useNavigate();
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    })
+    const json = await response.json();
+    console.log(json);
+
+    if (json.success) {
+      console.log("success")
+      // save the auth token and redirect from here to home. 
+      localStorage.setItem('authtoken', json.authtoken);
+      props.showAlert("Logged in successfully","success")
+      navigate('/')
+    } else {
+      console.log("danger")
+      props.showAlert("Invalid Credientials","danger")
+    }
+  }
+
+  const onchange = (e) => {
+    setcredientials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <>
+      <div className="container mt-3 t">
+        <div><p>If you have account Please Login Else Sign Up</p>
+          </div>
+        <form onSubmit={handleSubmit}  className="tt">
+          <div className="form-floating mb-3">
+            <input type="email" className="form-control ip" id="email" name='email' value={credentials.email} onChange={onchange} required />
+            <label htmlFor="email">Email address</label>
+          </div>
+          <div className="form-floating ip">
+            <input type="text" className="form-control ip" id="password" name='password' value={credentials.password} onChange={onchange} required />
+            <label htmlFor="password" >Password</label>
+          </div>
+          <div>
+          <button type="submit" className="btn btn-outline-success my-3" >Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Login
---
+++ client/src/Components/Employee/ApplyLeave.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import formatDistance from 'date-fns/formatDistance'
+import DevContext from '../../context/Employees/DevContext';
+
+const ApplyLeave = (props) => {
+    return (
+        <div className="maindev">
+            <div className="maindev card  text-center">
+            {/* Select the date for which employee want leaves after selecting send an notification to concern reporting manager */}
+
+            {/* A json file should be their to maintain the reporting manager of which each employee, that will be easy to send the notification to cocern people */}
+            
+            </div>
+        </div>
+    )
+}
+
+export default ApplyLeave
